2439732a6003652c33e61a015a7af28de3bc2d22
Devin <devin@thewebdev.ca>
2024-08-02 21:05:33 -0700

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frofer/',
+  base: '/frolfer/',
   plugins: [
     vue(),
     VitePWA({
